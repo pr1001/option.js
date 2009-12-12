cea5dee62aadf7d794f8fc19659868eb8e6ee778
Peter Robinett <peter@bubblefoundry.com>
2009-12-12 03:58:47 -0800

    Complete refactoring of the classes. Classes can now be called without the 'new' operator and inheritance is correctly handled by instanceof. The documentation also has lots more examples now.

--- option.js
+++ option.js
@@ -1,55 +1,65 @@
-function Option()
-{
-  this.__value = null;
-  this.isEmpty = function() { throw new Error() }
-  this.isDefined = function() { return !this.isEmpty() }
-  this.get = function() { throw new Error() }
-  this.getOrElse = function(alternative) {
-    return (this.isEmpty() ? alternative : this.get())
-  }
-  this.elements = function() { throw new Error() }
-  this.foreach = function(f) {
-    if (!this.isEmpty()) { return f(this.get()) }
-  }
-  this.map = function(f) {
-    return (this.isEmpty() ? None : new Some(f(this.get())))
-  }
-  this.flatMap = function(f) {
-    return (this.isEmpty() ? None : f(this.get()))
-  }
-  this.filter = function(f) {
-    if (this.isEmpty() || f(this.get())) {
-      return this
-    }
-    else {
-      return None
-    }
-  }
-  this.toArray = function() { return this.elements() }
-  this.toString = function() { return "Option" }
+var object = (function() {
+     function F() {}
+     return function(o) {
+        F.prototype = o;
+        F.prototype.__value = null;
+        F.prototype.isEmpty = function() { throw new Error() }
+        F.prototype.isDefined = function() { return !this.isEmpty() }
+        F.prototype.get = function() { throw new Error() }
+        F.prototype.getOrElse = function(alternative) {
+          return (this.isEmpty() ? alternative : this.get())
+        }
+        F.prototype.elements = function() { throw new Error() }
+        F.prototype.foreach = function(f) {
+          if (this.isDefined()) { f(this.get()) }
+        }
+        F.prototype.map = function(f) {
+          return (this.isEmpty() ? None : Some(f(this.get())))
+        }
+        F.prototype.flatMap = function(f) {
+          return (this.isEmpty() ? None : f(this.get()))
+        }
+        F.prototype.filter = function(f) {
+          if (this.isEmpty() || f(this.get())) {
+            return this
+          }
+          else {
+            return None
+          }
+        }
+        F.prototype.toArray = function() { return this.elements() }
+        F.prototype.toString = function() { return "Option" }
+        return new F();
+     };
+})();
+
+function Option(val, proto) {
+  var p = object(proto || Option.prototype);
+  p.__value = val;
+  return p;
 }
 
-
-/* (new Some) instanceof Option -> true */
-function Some(val) {
-  // if you try to create a Some with no value, you're actually dealing with a None
+function Some(val, proto) {
   if (typeof(val) == "undefined") { return None }
-  this.__value = val;
-  this.isEmpty = function() { return false }
-  this.get = function() { return this.__value }
-  this.elements = function() { return [this.get()] }
-  this.toString = function() { return "Some(" + this.get() + ")" }
+  var g = object(Option(val, proto || Some.prototype));
+  g.__value = val;
+  g.isEmpty = function() { return false }
+  g.get = function() { return this.__value }
+  g.elements = function() { return [this.get()] }
+  g.toString = function() { return "Some(" + this.get() + ")" }
+  return g;
 }
-Some.prototype = new Option
-Some.prototype.constructor = Some
+Some.prototype = object(Option.prototype);
+Some.__proto__ = Option.prototype;
 
-/* None instanceof Option -> true */
-function None() {
-  this.isEmpty = function() { return true }
-  this.elements = function() { return [] }
-  this.toString = function() { return "None" }
+function None(val, proto) {
+  var g = object(Option(val, proto || None.prototype));
+  g.isEmpty = function() { return true }
+  g.elements = function() { return [] }
+  g.toString = function() { return "None" }
+  return g;
 }
-None.prototype = new Option
-None.prototype.constructor = None
-/* ugly way to have None inherit from Option but still be a singleton */
-var None = new None()
+None.prototype = object(Option.prototype);
+None.__proto__ = Option.prototype;
+// create the None singleton
+None = None();
